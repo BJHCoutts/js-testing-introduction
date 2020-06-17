f288680b619fdd9ff22e13c8a56552be3426f6aa
Brian Coutts <bjhcoutts@gmail.com>
2020-06-16 23:49:56 -0700

    basic puppeteer test written

--- util.test.js
+++ util.test.js
@@ -1,4 +1,5 @@
 const {generateText, checkAndGenerate} = require('./util')
+const puppeteer = require('puppeteer')
 
 test('should output name and age', () => {
 	const text = generateText('Max', 29)
@@ -16,3 +17,26 @@ test('should generate a valid text output', () => {
 	const text = checkAndGenerate('Brian', 34)
 	expect(text).toBe('Brian (34 years old)')
 })
+
+test('should click around', async () => {
+	const broswer = await puppeteer.launch({
+		// headless: false,
+		// slowMo: 80,
+		// args: ['--window-size=1920, 1080']
+	})
+	const page = await broswer.newPage()
+	await page.goto(
+		'file:///C:/Users/basta/Desktop/js-testing-introduction/index.html'
+	)
+	await page.click('input#name')
+	await page.type('input#name', 'Brian')
+
+	await page.click('input#age')
+	await page.type('input#age', '28')
+
+	await page.click('#btnAddUser')
+
+	const finalText = await page.$eval('.user-item', el => el.textContent)
+	expect(finalText).toBe('Brian (28 years old)')
+}, 10000)
+// third argument to test is max timeout
